6a959fa3b7e98b5f5f573f252233b464c868e857
Freddy Rangel <frederick.rangel@gmail.com>
2017-02-26 21:48:05 -0800

    00-initialize-webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  context: path.resolve('js'),
+  output: {
+    path: path.resolve('build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [path.resolve('js')],
+        use: [{ loader: 'babel-loader' }]
+      }
+    ]
+  },
+  entry: './app.jsx'
+};
