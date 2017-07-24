48373099a674bcef054264cec271588f499b14a5
Freddy Rangel <frederick.rangel@gmail.com>
2017-07-24 01:44:04 -0700

    Update module 03

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -2,5 +2,5 @@ const smartMerge = require('webpack-merge').smart;
 const base       = require('./webpack.config.base.js');
 
 module.exports = smartMerge(base, {
-  devtool: 'eval'
+  devtool: 'eval-source-map'
 });
